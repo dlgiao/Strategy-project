df7c43ea2083fd95667e09ac65e3b7dbf95f724c
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-06 12:18:00 +0200

    Implement Javadoc

--- strategy_java/src/main/java/com/kmmania/Context.java
+++ strategy_java/src/main/java/com/kmmania/Context.java
@@ -3,13 +3,28 @@ package com.kmmania;
 import com.kmmania.strategy_training.AthleticTraining;
 import com.kmmania.strategy_training.DefaultTraining;
 
+/**
+ * Context class for applying different athletic training strategies.
+ * This class allows clients to set and switch between different training strategies
+ * and execute training sessions accordingly.
+ */
 public class Context {
     private AthleticTraining athleticTraining = new DefaultTraining();
 
+    /**
+     * Sets the athletic training strategy to be used.
+     *
+     * @param athleticTraining The athletic training strategy to be set.
+     */
     public void setAthleticTraining(AthleticTraining athleticTraining) {
         this.athleticTraining = athleticTraining;
     }
 
+    /**
+     * Executes a training session using the current athletic training strategy.
+     * This method prints a header indicating the start of the training session,
+     * executes the training using the current strategy, and prints a footer.
+     */
     public void doTraining() {
         System.out.println("=== Training session ===");
         athleticTraining.train();

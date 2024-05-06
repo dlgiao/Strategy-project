3401439b8659cf0b7fc039555205d43e1afce6c1
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-06 12:14:50 +0200

    Implement Javadoc

--- strategy_java/src/main/java/com/kmmania/strategy_training/JumpTraining.java
+++ strategy_java/src/main/java/com/kmmania/strategy_training/JumpTraining.java
@@ -1,12 +1,26 @@
 package com.kmmania.strategy_training;
 
+/**
+ * Implementation of the {@link AthleticTraining} interface representing
+ * jump training strategy.
+ * This class allows athletes to perform training sessions for jump events.
+ */
 public class JumpTraining implements AthleticTraining {
     private final TrainingSession trainingSession;
 
+    /**
+     * Constructs a new instance of JumpTraining with the provided TrainingSession.
+     *
+     * @param trainingSession The training session details (date, time slot, place).
+     */
     public JumpTraining(TrainingSession trainingSession) {
         this.trainingSession = trainingSession;
     }
 
+    /**
+     * Method to perform the jump training strategy.
+     * This method prints details of the training session, including date, time slot, and place.
+     */
     @Override
     public void train() {
         System.out.println("*** Jump training ***");

2e8c9a50e776ce5a5cbe5c92f57a60a3532a781e
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-06 11:32:53 +0200

    Extend BaseTrainingTest

--- strategy_java/src/test/java/com/kmmania/strategy_training/DefaultTrainingTest.java
+++ strategy_java/src/test/java/com/kmmania/strategy_training/DefaultTrainingTest.java
@@ -1,32 +1,11 @@
 package com.kmmania.strategy_training;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class DefaultTrainingTest {
-
-    // Utility field and stream for capturing System.out.println output
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
-
-    // Setup capturing of System.out.println output
-    @BeforeEach
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    // Reset System.out.println output capturing
-    @AfterEach
-    public void restoreStreams() {
-        System.setOut(originalOut);
-    }
+public class DefaultTrainingTest extends BaseTrainingTest {
 
     @Test
     @DisplayName("train() method should print DefaultTraining message")
@@ -40,10 +19,5 @@ public class DefaultTrainingTest {
         // Then
         assertThat(getStandardOutContent()).contains("*** Default training ***");
         assertThat(getStandardOutContent()).contains("...");
-    }
-
-    // Utility method to capture System.out.println output
-    private String getStandardOutContent() {
-        return outContent.toString().trim();
     }
 }

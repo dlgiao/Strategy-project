6d58b094982594b1634199456b6122b02503bddb
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-06 12:09:45 +0200

    Implement Javadoc

--- strategy_java/src/main/java/com/kmmania/strategy_training/DefaultTraining.java
+++ strategy_java/src/main/java/com/kmmania/strategy_training/DefaultTraining.java
@@ -1,6 +1,16 @@
 package com.kmmania.strategy_training;
 
+/**
+ * Default implementation of the {@link AthleticTraining} interface.
+ * This class represents the default training strategy.
+ */
 public class DefaultTraining implements AthleticTraining {
+
+    /**
+     * Method to perform the default training strategy.
+     * This method prints a message indicating that default training
+     * is being performed.
+     */
     @Override
     public void train() {
         System.out.println("*** Default training ***");

9895e769fce7a8e29cdb19ac315a59e4dcc7ebc6
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-06 12:08:38 +0200

    Implement Javadoc

--- strategy_java/src/main/java/com/kmmania/strategy_training/AthleticTraining.java
+++ strategy_java/src/main/java/com/kmmania/strategy_training/AthleticTraining.java
@@ -1,5 +1,16 @@
 package com.kmmania.strategy_training;
 
+/**
+ * Interface representing different athletic training strategies.
+ * Implementations of this interface encapsulate various training methods
+ * that can be applied to athletes.
+ */
 public interface AthleticTraining {
+
+    /**
+     * Method to perform the specific training strategy.
+     * Implementing classes should define the behavior of this method
+     * according to the particular training strategy they represent.
+     */
     void train();
 }

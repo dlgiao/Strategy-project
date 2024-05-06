851e24609aac24594a78a56630daeef47255850d
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-06 11:34:28 +0200

    Extend BaseTrainingTest

--- strategy_java/src/test/java/com/kmmania/strategy_training/DistanceTrainingTest.java
+++ strategy_java/src/test/java/com/kmmania/strategy_training/DistanceTrainingTest.java
@@ -1,34 +1,15 @@
 package com.kmmania.strategy_training;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class DistanceTrainingTest {
-    // Utility field and stream for capturing System.out.println output
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
-
-    // Setup capturing of System.out.println output
-    @BeforeEach
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    // Reset System.out.println output capturing
-    @AfterEach
-    public void restoreStreams() {
-        System.setOut(originalOut);
-    }
+class DistanceTrainingTest extends BaseTrainingTest {
 
     @Test
     @DisplayName("train() method should print DistanceTraining message")
@@ -49,10 +30,5 @@ class DistanceTrainingTest {
         assertThat(getStandardOutContent()).contains("Date: " + LocalDate.now());
         assertThat(getStandardOutContent()).contains("Time slot: Evening");
         assertThat(getStandardOutContent()).contains("Place: Louis Lumiere");
-    }
-
-    // Utility method to capture System.out.println output
-    private String getStandardOutContent() {
-        return outContent.toString().trim();
     }
 }

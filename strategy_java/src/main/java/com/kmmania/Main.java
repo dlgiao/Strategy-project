d1b70180af1c3bbf907550c5f0afe291f3497817
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-06 11:05:51 +0200

    Implement different training strategies

--- strategy_java/src/main/java/com/kmmania/Main.java
+++ strategy_java/src/main/java/com/kmmania/Main.java
@@ -1,7 +1,28 @@
 package com.kmmania;
 
+import com.kmmania.strategy_training.*;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Context context = new Context();
+        context.doTraining();
+
+        TrainingSession trainingSession1 = new TrainingSession(new Date(), "Afternoon", "Pershing");
+        context.setAthleticTraining(new SprintTraining(trainingSession1));
+        context.doTraining();
+
+        TrainingSession trainingSession2 = new TrainingSession(new Date(), "Morning", "Stephane Diagana");
+        context.setAthleticTraining(new ThrowTraining(trainingSession2));
+        context.doTraining();
+
+        TrainingSession trainingSession3 = new TrainingSession(new Date(), "Evening", "Louis Lumiere");
+        context.setAthleticTraining(new DistanceTraining(trainingSession3));
+        context.doTraining();
+
+        TrainingSession trainingSession4 = new TrainingSession(new Date(), "Evening", "Alain Mimoun");
+        context.setAthleticTraining(new JumpTraining(trainingSession4));
+        context.doTraining();
     }
 }

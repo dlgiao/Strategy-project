a53f06cb6c74d76bf0bf46af2be347af917efe08
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-06 12:13:37 +0200

    Implement Javadoc

--- strategy_java/src/main/java/com/kmmania/strategy_training/DistanceTraining.java
+++ strategy_java/src/main/java/com/kmmania/strategy_training/DistanceTraining.java
@@ -1,12 +1,26 @@
 package com.kmmania.strategy_training;
 
+/**
+ * Implementation of the {@link AthleticTraining} interface representing
+ * distance training strategy.
+ * This class allows athletes to perform training sessions for distance events.
+ */
 public class DistanceTraining implements AthleticTraining {
     private final TrainingSession trainingSession;
 
+    /**
+     * Constructs a new instance of DistanceTraining with the provided TrainingSession.
+     *
+     * @param trainingSession The training session details (date, time slot, place).
+     */
     public DistanceTraining(TrainingSession trainingSession) {
         this.trainingSession = trainingSession;
     }
 
+    /**
+     * Method to perform the distance training strategy.
+     * This method prints details of the training session, including date, time slot, and place.
+     */
     @Override
     public void train() {
         System.out.println("*** Distance training ***");
--- strategy_java/src/main/java/com/kmmania/strategy_training/TrainingSession.java
+++ strategy_java/src/main/java/com/kmmania/strategy_training/TrainingSession.java
@@ -2,6 +2,10 @@ package com.kmmania.strategy_training;
 
 import java.time.LocalDate;
 
+/**
+ * Represents a training session for athletic training strategies.
+ * This record encapsulates details of a training session including the date, time slot, and place.
+ */
 public record TrainingSession(LocalDate date, String timeSlot, String place) {
 }
 

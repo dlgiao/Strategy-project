8b6b4616756228f1ed6346ee792542c271040a88
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-06 10:26:09 +0200

    Implement DistanceTraining implementation

---
+++ strategy_java/src/main/java/com/kmmania/strategy_training/DistanceTraining.java
@@ -0,0 +1,17 @@
+package com.kmmania.strategy_training;
+
+public class DistanceTraining implements AthleticTraining {
+    private final TrainingSession trainingSession;
+
+    public DistanceTraining(TrainingSession trainingSession) {
+        this.trainingSession = trainingSession;
+    }
+
+    @Override
+    public void train() {
+        System.out.println("*** Distance training ***");
+        System.out.println("Date: " + this.trainingSession.date());
+        System.out.println("Time slot: " + this.trainingSession.timeSlot());
+        System.out.println("Place: " + this.trainingSession.place());
+    }
+}

fb92f4910228cef24544a7dd5e6ba92d00bd0b2b
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-07 12:44:05 +0200

    Implement TrainingSession

--- strategy_go/strategy_training/strategy_training.go
+++ strategy_go/strategy_training/strategy_training.go
@@ -1,6 +1,9 @@
 package strategy_training
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type AthleticTraining interface {
 	Train()
@@ -10,4 +13,10 @@ type DefaultTraining struct{}
 func (dt *DefaultTraining) Train() {
 	fmt.Println("*** Default training ***")
 	fmt.Println("...")
+}
+
+type TrainingSession struct {
+	Date     time.Time
+	TimeSlot string
+	Place    string
 }

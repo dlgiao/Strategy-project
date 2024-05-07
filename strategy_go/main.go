c314d2ff002cf543b6272e4c783698c06f61e933
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-07 13:04:32 +0200

    Implement main

---
+++ strategy_go/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"kmmania.com/strategy_go/strategy_training"
+	"time"
+)
+
+func main() {
+	context := Context{AthleticTraining: &strategy_training.DefaultTraining{}}
+	context.DoTraining()
+
+	sprintSession := strategy_training.TrainingSession{
+		Date:     time.Time{},
+		TimeSlot: "Morning",
+		Place:    "Insep",
+	}
+	context.AthleticTraining = &strategy_training.SprintTraining{
+		TrainingSession: sprintSession,
+	}
+	context.DoTraining()
+
+	distanceSession := strategy_training.TrainingSession{
+		Date:     time.Time{},
+		TimeSlot: "Evening",
+		Place:    "Louis Lumiere",
+	}
+	context.AthleticTraining = &strategy_training.DistanceTraining{
+		TrainingSession: distanceSession,
+	}
+	context.DoTraining()
+
+	jumpSession := strategy_training.TrainingSession{
+		Date:     time.Time{},
+		TimeSlot: "Evening",
+		Place:    "Alain Mimoun",
+	}
+	context.AthleticTraining = &strategy_training.JumpTraining{
+		TrainingSession: jumpSession,
+	}
+	context.DoTraining()
+
+	throwSession := strategy_training.TrainingSession{
+		Date:     time.Time{},
+		TimeSlot: "Afternoon",
+		Place:    "Pershing",
+	}
+	context.AthleticTraining = &strategy_training.ThrowTraining{
+		TrainingSession: throwSession,
+	}
+	context.DoTraining()
+}

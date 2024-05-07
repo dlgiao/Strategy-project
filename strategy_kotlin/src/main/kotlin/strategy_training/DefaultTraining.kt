56a75026732c03f5c5a968295b7baec336220448
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-07 10:36:11 +0200

    Implement DefaultTraining

---
+++ strategy_kotlin/src/main/kotlin/strategy_training/DefaultTraining.kt
@@ -0,0 +1,8 @@
+package com.kmmania.strategy_training
+
+class DefaultTraining : AthleticTraining {
+    override fun train() {
+        println("*** Default training ***")
+        println("...")
+    }
+}

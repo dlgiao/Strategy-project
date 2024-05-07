5a08ac3e8e77dd208fe8a219de22e823987bf867
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-07 12:46:41 +0200

    Implement DistanceTraining

--- strategy_go/strategy_training/strategy_training.go
+++ strategy_go/strategy_training/strategy_training.go
@@ -15,6 +15,17 @@ func (dt *DefaultTraining) Train() {
 	fmt.Println("...")
 }
 
+type DistanceTraining struct {
+	TrainingSession TrainingSession
+}
+
+func (dt *DistanceTraining) Train() {
+	fmt.Println("*** Distance training ***")
+	fmt.Printf("Date: %s\n", dt.TrainingSession.Date)
+	fmt.Printf("Time slot: %s\n", dt.TrainingSession.TimeSlot)
+	fmt.Printf("Place: %s\n", dt.TrainingSession.Place)
+}
+
 type TrainingSession struct {
 	Date     time.Time
 	TimeSlot string

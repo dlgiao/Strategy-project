14643ca899e7b0bbbaea46262c5dd0fcf207ac3e
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-07 10:30:28 +0200

    Update

--- strategy_kotlin/build.gradle.kts
+++ strategy_kotlin/build.gradle.kts
@@ -1,16 +1,30 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.jetbrains.dokka") version "1.9.20"
 }
 
 group = "com.kmmania"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
 repositories {
     mavenCentral()
 }
 
+application {
+    mainClass = "com.kmmania.MainKt"
+}
+
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
 }
 
 tasks.test {

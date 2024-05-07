0c81be8e4b54ff2f4ae0d0245d56b6b6cb441711
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-07 11:48:52 +0200

    Implement KDoc

--- strategy_kotlin/src/main/kotlin/strategy_training/DefaultTraining.kt
+++ strategy_kotlin/src/main/kotlin/strategy_training/DefaultTraining.kt
@@ -1,6 +1,27 @@
+/*
+ * Strategy project.
+ * Dac Linh Giao NGUYEN
+ * License MIT
+ *
+ * Strategy project in Kotlin.
+ * DefaultTraining.kt
+ */
 package com.kmmania.strategy_training
 
+/**
+ * Default implementation of the [AthleticTraining] interface.
+ * This class represents the default training strategy.
+ *
+ * @author Dac Linh Giao NGUYEN
+ * @since 0.1
+ * @see AthleticTraining
+ */
 class DefaultTraining : AthleticTraining {
+
+    /**
+     * Method to perform the default training strategy.
+     * This method prints a message indicating that default training is being performed.
+     */
     override fun train() {
         println("*** Default training ***")
         println("...")

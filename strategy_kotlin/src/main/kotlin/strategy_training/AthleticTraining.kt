62511b19d31eda95647d05789d15b24beac657c2
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-07 11:46:40 +0200

    Update KDoc

--- strategy_kotlin/src/main/kotlin/strategy_training/AthleticTraining.kt
+++ strategy_kotlin/src/main/kotlin/strategy_training/AthleticTraining.kt
@@ -1,5 +1,26 @@
+/*
+ * Strategy project.
+ * Dac Linh Giao NGUYEN
+ * License MIT
+ *
+ * Strategy project in Kotlin.
+ * AthleticTraining.kt
+ */
 package com.kmmania.strategy_training
 
+/**
+ * Interface representing different athletic training strategies.
+ * Implementations of this interface encapsulate various training sessions.
+ *
+ * @author Dac Linh Giao NGUYEN
+ * @since 0.1
+ */
 interface AthleticTraining {
+
+    /**
+     * Method to perform the specific training strategy.
+     * Implementing classes should define the behavior of this method
+     * according to the particular training strategy they represent.
+     */
     fun train()
 }

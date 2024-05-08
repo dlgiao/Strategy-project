19bc191a993f5bf768f4d27244bd3569ae08ea85
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-09 00:06:25 +0200

    Implement TestDistanceTraining_Train

--- strategy_go/strategy_training/strategy_training_test.go
+++ strategy_go/strategy_training/strategy_training_test.go
@@ -17,8 +17,8 @@ func TestDefaultTraining_Train(t *testing.T) {
 	})
 
 	// Then
-	expected := "*** Default training ***\n...\n"
-	assert.Equal(t, expected, output, "Expected output %s, but got %s", expected, output)
+	expectedOutput := "*** Default training ***\n...\n"
+	assert.Equal(t, expectedOutput, output, "Expected output %s, but got %s", expectedOutput, output)
 }
 
 func TestSprintTraining_Train(t *testing.T) {
@@ -37,9 +37,32 @@ func TestSprintTraining_Train(t *testing.T) {
 	})
 
 	// Then
-	expected := "*** Sprint training ***\n" +
+	expectedOutput := "*** Sprint training ***\n" +
 		"Date: 2024-05-05\n" + // Utilisez une date fixe pour l'attente
 		"Time slot: Morning\n" +
 		"Place: Insep\n"
-	assert.Equal(t, expected, output, "Expected output %s, but got %s", expected, output)
+	assert.Equal(t, expectedOutput, output, "Expected output %s, but got %s", expectedOutput, output)
+}
+
+func TestDistanceTraining_Train(t *testing.T) {
+	// Given
+	distanceTraining := DistanceTraining{
+		TrainingSession: TrainingSession{
+			Date:     time.Date(2024, 5, 5, 0, 0, 0, 0, time.UTC), // Utilisez une date fixe pour le test
+			TimeSlot: "Evening",
+			Place:    "Louis Lumiere",
+		},
+	}
+
+	// When
+	output := utils.CaptureOutput(func() {
+		distanceTraining.Train()
+	})
+
+	// Then
+	expectedOutput := "*** Distance training ***\n" +
+		"Date: 2024-05-05\n" + // Utilisez une date fixe pour l'attente
+		"Time slot: Evening\n" +
+		"Place: Louis Lumiere\n"
+	assert.Equal(t, expectedOutput, output, "Expected output %s, but got %s", expectedOutput, output)
 }

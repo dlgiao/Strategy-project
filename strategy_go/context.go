79a31728a40b63606d82377fb45a1f720348b9af
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-09 00:45:59 +0200

    Implement documentation

--- strategy_go/context.go
+++ strategy_go/context.go
@@ -5,10 +5,17 @@ import (
 	"kmmania.com/strategy_go/strategy_training"
 )
 
+// Context struct for applying different athletic training strategies.
+// This struct allows clients to set and switch between different training strategies
+// and execute training sessions accordingly.
 type Context struct {
+	// The athletic training strategy to be set.
 	AthleticTraining strategy_training.AthleticTraining
 }
 
+// DoTraining executes a training session using the current athletic training strategy.
+// This method prints a header indicating the start of the training session,
+// executes the training using the current strategy, and prints a footer.
 func (ctx *Context) DoTraining() {
 	fmt.Println("=== Training session ===")
 	ctx.AthleticTraining.Train()

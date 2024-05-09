9f5b94536f48fb0758832efd94c60bf289529fea
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-10 00:10:32 +0200

    Implement DefaultTraining unit tests

---
+++ strategy_python/tests/strategy_training/test_default_training.py
@@ -0,0 +1,17 @@
+from src.strategy_training.DefaultTraining import DefaultTraining
+
+
+class TestDefaultTraining:
+
+    def setup_method(self, method):
+        print(f"Setting up {method}")
+        self.default_training = DefaultTraining()
+
+    def teardown_method(self, method):
+        print(f"Tearing down {method}")
+        del self.default_training
+
+    def test_train(self, capsys):
+        self.default_training.train()
+        captured = capsys.readouterr()
+        assert captured.out == "*** Default training ***\n...\n"

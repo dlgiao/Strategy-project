f44c5a4062e4921ff4a0ee61ad4f91044b3d2156
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-10 00:03:48 +0200

    Implement DefaultTraining

---
+++ strategy_python/src/strategy_training/DefaultTraining.py
@@ -0,0 +1,7 @@
+from src.strategy_training.AthleticTraining import AthleticTraining
+
+
+class DefaultTraining(AthleticTraining):
+    def train(self):
+        print("*** Default training ***")
+        print("...")

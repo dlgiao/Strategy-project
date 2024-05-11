b53b509b21d664bbf7a023beb6470070760e66b8
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-11 15:06:45 +0200

    Implement documentation

--- strategy_python/src/strategy_training/DistanceTraining.py
+++ strategy_python/src/strategy_training/DistanceTraining.py
@@ -1,11 +1,61 @@
+"""
+Strategy Training Project
+
+This project demonstrates the implementation of various athletic training strategies using Python.
+
+The project showcases the Strategy Design Pattern, where different training strategies are encapsulated into separate
+classes implementing a common interface. It allows athletes to perform training sessions for different types of athletic
+events, such as sprinting, distance running, jumping, and throwing.
+
+Classes:
+    AthleticTraining: Interface representing different athletic training strategies.
+    Context: Class providing a context for executing training sessions using different strategies.
+    TrainingSession: Class representing details of a training session including date, time slot, and place.
+    DefaultTraining: Default implementation of the AthleticTraining interface.
+    SprintTraining: Implementation of the AthleticTraining interface for sprint training.
+    DistanceTraining: Implementation of the AthleticTraining interface for distance training.
+    JumpTraining: Implementation of the AthleticTraining interface for jump training.
+    ThrowTraining: Implementation of the AthleticTraining interface for throw training.
+    Main: Contains the `main` method to demonstrate the usage of different training strategies.
+
+Usage:
+    Implementation of the AthleticTraining interface representing distance training strategy.
+    This class allows training planification for distance sessions.
+
+Author: Dac Linh Giao NGUYEN
+Date: 2024
+
+"""
 from src.strategy_training.AthleticTraining import AthleticTraining
 
 
 class DistanceTraining(AthleticTraining):
+    """
+    Implementation of the AthleticTraining interface representing distance training strategy.
+
+    This class allows training planification for distance sessions.
+
+    Attributes:
+        training_session (TrainingSession): Details of the training session including date, time slot, and place.
+
+    Methods:
+        train(): Method implementing the distance training behavior.
+    """
     def __init__(self, training_session):
+        """
+        Initialize the DistanceTraining instance.
+
+        Args:
+            training_session (TrainingSession): Details of the training session including date, time slot, and place.
+        """
         self.training_session = training_session
 
     def train(self):
+        """
+        Perform distance training.
+
+        Prints information about the training session.
+        """
         print("*** Distance training ***")
         print(f"Date: {self.training_session.date}")
         print(f"Time slot: {self.training_session.time_slot}")
